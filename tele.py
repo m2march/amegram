37f92a50a61a56cb10f3c6dd29d9df2992f0e50c
m2march <m2.march@gmail.com>
2025-05-12 01:34:38 +0100

    Recording working

--- tele.py
+++ tele.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO  
 import keybow
-import time
-import os
-import subprocess
-import pygame
-import time
 import logging
+import time
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, ContextTypes, CommandHandler,
                           MessageHandler, PollHandler, filters)
 import configparser
+import sounddevice as sd
+import pydub
+from scipy.io import wavfile
+import numpy as np
+import asyncio
+from pathlib import Path
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -31,14 +33,53 @@ colors = {
     PLAY_KEY : PLAY_COLOR,
 }
 
+sd.default.device = [0, 1]
+recording_rate = 44000
+recording_time = 15
+target_user = 76053031 #"@M2March"
+
+new_outgoing_message = asyncio.Event()
+
 @keybow.on()
 def handle_key(index, state):
     if state:
         if index == PLAY_KEY:
-            try:
-                p = subprocess.Popen(["mplayer", "voice.ogg"])
-            except ValueError as ve:
-                print(ve)
+            logging.info('Playing sound')
+            a = pydub.AudioSegment.from_file('voice.ogg')
+            sd.play(a.get_array_of_samples())
+            logging.info('Playback done')
+        elif index == REC_KEY:
+            logging.info('Recording sound')
+            start_time = time.time()
+            r = sd.rec(recording_time * recording_rate, 
+                       samplerate=recording_rate,
+                       channels=1, blocking=False)
+            logging.info('Recording started')
+            while time.time() - start_time < recording_time:
+                passed_time = time.time() - start_time
+                key_alpha = (np.cos(passed_time / (2 * np.pi)) + 1) / 2
+                key_color = np.array(colors[REC_KEY]) * key_alpha
+                key_color = [int(x) for x in key_color]
+                keybow.set_led(REC_KEY+3, *key_color)
+                keybow.show()
+                time.sleep(0.1)
+            logging.info('Recording ended')
+            keybow.set_led(REC_KEY+3, *colors[REC_KEY])
+            keybow.show()
+            wavfile.write('rec.wav', recording_rate, r)
+            a = pydub.AudioSegment.from_file('rec.wav')
+            a.export('rec.ogg', format='ogg')
+            logging.info('Recording done')
+            new_outgoing_message.set()
+
+
+async def send_voice_note(bot):
+    while True:
+        await new_outgoing_message.wait()
+        new_outgoing_message.clear()
+        logging.info(f'Sending voice message to: {target_user}')
+        await bot.send_voice(target_user, Path('rec.ogg'))
+        logging.info(f'Done sending voice message')
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -74,9 +115,12 @@ async def message_log(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def voice_echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info(f'Voice from:{update.message.from_user} :: {update.message.audio.duration}s')
 
-if __name__ == '__main__':
+async def main():
     conf = configparser.ConfigParser()
     conf.read('tele.conf')
+
+    keybow.set_led(REC_KEY+3, *colors[REC_KEY])
+    keybow.show()
 
     application = ApplicationBuilder().token(conf['bot']['bot_key']).build()
     
@@ -86,5 +130,18 @@ if __name__ == '__main__':
 
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
-    
-    application.run_polling()
+
+    async with application:
+        await application.start()
+        await application.updater.start_polling()
+
+        send_task = asyncio.create_task(send_voice_note(application.bot))
+
+        await send_task
+
+        await application.updater.stop()
+        await application.stop()
+        await application.shutdown()
+
+if __name__ == '__main__':
+    asyncio.run(main())
