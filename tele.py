7c54b8fd8ee5fbe358d410206d23794696115895
March <m2.march@gmail.com>
2025-07-27 06:06:19 +0100

    Date logs

--- tele.py
+++ tele.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import keybow
 import logging
 import time
+import datetime
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, ContextTypes, CommandHandler,
                           MessageHandler, PollHandler, filters)
@@ -101,6 +102,7 @@ def blinking_light(cond_f, key, color_arr, freq=2, sleep_time=0.1):
 
 
 async def incoming_light(context):
+    logging.info('Started job: "incoming_light"')
     await new_incoming_message.wait()
     blinking_light(lambda t : new_incoming_message.is_set(),
                    PLAY_KEY, colors[PLAY_KEY], freq=0.25)
@@ -110,6 +112,7 @@ async def incoming_light(context):
 
 
 async def record_voice_note(context):
+    logging.info('Started job: "record_voice_note"')
     await recording_message.wait()
     start_time = time.time()
     r = sd.rec(max_recording_time * recording_rate, 
@@ -155,15 +158,16 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.voice is not None:
         logging.info(f'Voice from:{update.message.from_user} :: {update.message.voice.duration}s')
 
-        file_id = update.message.voice.file_id
-        new_file = await context.bot.get_file(file_id)
-        await new_file.download_to_drive('voice.ogg')
+        if update.message.from_user == target_user:
+            file_id = update.message.voice.file_id
+            new_file = await context.bot.get_file(file_id)
+            await new_file.download_to_drive('voice.ogg')
 
-        a = pydub.AudioSegment.from_file('voice.ogg')
-        a = a.apply_gain(amp_correction)
-        a.export('voice.wav', format='wav')
+            a = pydub.AudioSegment.from_file('voice.ogg')
+            a = a.apply_gain(amp_correction)
+            a.export('voice.wav', format='wav')
 
-        new_incoming_message.set()
+            new_incoming_message.set()
     else:
         logging.info(f'Message from:{update.message.from_user} :: {update.message.text}')
         keybow.set_led(MID_KEY+3, *colors[MID_KEY])
@@ -184,11 +188,6 @@ def main():
     conf = configparser.ConfigParser()
     conf.read('tele.conf')
 
-    keybow.set_led(REC_KEY+3, *colors[REC_KEY])
-    keybow.set_led(PLAY_KEY+3, *adjust_color_alpha(colors[PLAY_KEY], OFF_ALPHA))
-    keybow.show()
-    inc_volume()
-
     application = ApplicationBuilder().token(conf['bot']['bot_key']).build()
     
     start_handler = CommandHandler('start', start)
@@ -198,10 +197,19 @@ def main():
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
 
-    application.job_queue.run_once(send_voice_note, 0.1)
-    application.job_queue.run_once(incoming_light, 0.1)
-    application.job_queue.run_once(record_voice_note, 0.1)
+    logging.info(time.asctime())
 
+    td = datetime.timedelta(hours=0, seconds=1)
+
+    application.job_queue.run_once(send_voice_note, td)
+    application.job_queue.run_once(incoming_light, td)
+    application.job_queue.run_once(record_voice_note, td)
+
+    keybow.set_led(REC_KEY+3, *colors[REC_KEY])
+    keybow.set_led(PLAY_KEY+3, *adjust_color_alpha(colors[PLAY_KEY], OFF_ALPHA))
+    keybow.show()
+    inc_volume()
+    
     application.run_polling()
 
 if __name__ == '__main__':
